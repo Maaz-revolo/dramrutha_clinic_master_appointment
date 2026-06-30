30e1ae3e620a6ab97f761cf3f546851da7eb2ee1
md0011 <mayurd0303@gmail.com>
2026-06-30 12:35:06 +0530

    thankyou page

--- thank-you.php
+++ thank-you.php
@@ -5,10 +5,11 @@
 		<div class="thankyou-sec-cont-c">
 			<img src="assets/images/thnx.svg" alt="" class="floating-b">
 			<h3>
-				Thank you for filling out the form.
-				<span>Our representative will get in touch with you.</span>
+				Thank you for reaching out to us.
+				<span>Our team will contact you shortly.</span>
 			</h3>
-			<a href="index.php?campaigntype=<?= $campaigntype ?>&utm_campaign=<?= $utm_campaign ?>&utm_term=<?= $utm_term ?>&utm_adname=<?= $adName ?>&utm_matchtype=<?= $utm_matchtype ?>&utm_adposition=<?= $utm_adposition ?>&utm_devicemodel=<?= $utm_devicemodel ?>&utm_content=<?= $utm_content ?>&utm_location=<?= $utm_location ?>" class="f-btn"> Back to Home</a>
+			<p>For immediate assistance, you may call us by clicking the button below.</p>
+			<a href="tel:09849792111" class="f-btn"> Call us at 098497 92111</a>		
 		</div>
 	</div>
 </section>
